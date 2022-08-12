b0d40563699f43eb7e3611ed9642a3e06c211b0f
Jakub Krojec <jakub.krojec@gmail.com>
2022-08-12 17:51:54 +0200

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,43 @@
+# install xcode build tools
+
+xcode-select --install
+
+# install homebrew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install homebrew formulae
+
+# install homebrew casks
+
+# setup bash_profile and other dot files
+
+cd ~
+
+curl -O [LINK TO .bashprofile GIST]
+
+curl -O [LINK TO .gitignore GIST]
+
+# configure git
+
+git config --global user.name [ENTER NAME]
+
+git config --global user.email [ENTER EMAIL]
+
+git config --global core.editor nano
+
+# install node version manager
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/HEAD/install.sh | bash
+
+nvm install lts
+nvm install latest
+
+# global npm installs
+
+nvm use latest
+npm install -g lite-server gitignore license
+
+# create dev folder
+
+mkdir ~/dev
